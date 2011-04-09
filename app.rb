956056f57558146bf1dccfdf4a3e2bed131e1a31
Patrick Sinclair <metade@gmail.com>
2011-04-09 18:36:08 +0100

    Adding flickr source backgrounds

--- app.rb
+++ app.rb
@@ -1,14 +1,41 @@
 require 'sinatra'
 require 'ostruct'
 require 'json'
+require 'flickraw'
 require 'active_support/inflector'
 
-$countries = JSON.parse(open('test.json').read)
+configure do
+  FlickRaw.api_key = ENV['flickr_api_key']
+  $countries = JSON.parse(open('test.json').read)
+end
+
 class Country < OpenStruct
   def self.find_by_slug(slug)
     country = Country.new($countries[slug])
     country.name = slug.titleize
     country
+  end
+  
+  def poster_image
+    unless @poster_image
+      photos = flickr.photos.search(
+        :tags => name, 
+        :is_commons => true, 
+        # :content_type => 1,
+        # :sort => 'interestingness-desc',
+        :per_page => 1)
+      if photos.any?
+        sizes = flickr.photos.getSizes(:photo_id => photos.first.id)
+        largest = sizes.sort { |a,b| b['width'].to_i <=> a['width'].to_i }.first
+        p photos.first.to_hash
+        @poster_image = OpenStruct.new(photos.first.to_hash)
+        @poster_image.url = "http://www.flickr.com/photos/#{@poster_image.owner}/#{@poster_image.id}/"
+        @poster_image.image_url = largest['source']
+      else
+        @poster_image = OpenStruct.new()
+      end
+    end
+    @poster_image
   end
 end
 
