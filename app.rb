e6c638b22b18b5bbe2f9999ed1b36b329ca697a9
Patrick Sinclair <metade@gmail.com>
2011-04-09 17:38:08 +0100

    tidying up code

--- app.rb
+++ app.rb
@@ -3,26 +3,7 @@ require 'ostruct'
 require 'json'
 require 'activesupport'
 
-$countries = JSON.parse(open('test.json').read) 
-
-def conditions_mentioning_country(country)
-  endpoint = 'http://localhost:8080/sparql/'
-  
-  p "   #{country}"
-  query = %[
-    PREFIX rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#>
-    PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#>
-
-    SELECT ?conditions WHERE {
-      ?conditions rdf:type <http://data.linkedgov.org/data/hmrc/tariff/conditions> .
-      ?conditions <http://data.linkedgov.org/syntax/hmrc/tariff/syntax#for> "Sudan" .
-    } 
-  ]
-  response = RestClient.post(endpoint, :query => query)
-  xml = Nokogiri::XML(response.to_str)
-  xml.xpath('//sparql:result', 'sparql' => 'http://www.w3.org/2005/sparql-results#').count
-end
-
+$countries = JSON.parse(open('test.json').read)
 class Country < OpenStruct
   def self.find_by_slug(slug)
     country = Country.new($countries[slug])
