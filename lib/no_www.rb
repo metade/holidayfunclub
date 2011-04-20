6df392285e0ec03503fd965866986a9e130591d1
Patrick Sinclair <metade@gmail.com>
2011-04-20 22:56:21 +0100

    adding caching for redirects

--- lib/no_www.rb
+++ lib/no_www.rb
@@ -7,9 +7,9 @@ class NoWww
     request = Rack::Request.new(env)
     
     if request.host =~ /^www\./
-      [301, {"Location" => request.url.sub("//www.", "//")}, self]
+      [301, {"Cache-Control" => "public, max-age=3600", "Location" => request.url.sub("//www.", "//")}, self]
     elsif request.host =~ /heroku\.com$/
-      [301, {"Location" => request.url.sub(".heroku.com/", ".com/")}, self]
+      [301, {"Cache-Control" => "public, max-age=3600", "Location" => request.url.sub(".heroku.com/", ".com/")}, self]
     else
       @app.call(env)
     end
